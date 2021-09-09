4ffdbd00f0ca76208655dcdfa57db0ad2bb7f6c0
a18980620 <Masters5>
2021-09-10 00:25:02 +0300

    task 2

--- 2_if2.py
+++ 2_if2.py
@@ -15,12 +15,31 @@
 
 """
 
+
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-    
+
+    def string_compare(string1, string2):
+        if type(string1) != str or type(string2) != str:
+            return 0
+        elif string1 == string2:
+            return 1
+        elif len(string1) > len(string2):
+            return 2
+        elif string1 != string2 and string2 == "learn":
+            return 3
+        else:
+            return -1
+
+    print("Один из параметров не строка:", string_compare(1, "text"))
+    print("Две одинаковые строки:", string_compare('text', "text"))
+    print("Первая строка длиннее:", string_compare('text1', "text"))
+    print("Вторая строка learn:", string_compare('text1', "learn"))
+    print("Иначе:", string_compare('text', "text2"))
+
+
 if __name__ == "__main__":
     main()
